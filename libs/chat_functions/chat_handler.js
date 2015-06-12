3237b6188be683df4c36c464282eb80ad7d07da7
Ryan Flaherty <Ryan.Flaherty2@gmail.com>
2015-06-12 17:48:16 -0500

    Got wordbook working.

--- libs/chat_functions/chat_handler.js
+++ libs/chat_functions/chat_handler.js
@@ -1,7 +1,6 @@
 module.exports = function(twitch_client, channel, user, message){
 		console.log("count "+ count);
 		count = count+1;
-		twitch_client.action(channel, "WOO");
 };
 
 var count = 0;
--- libs/chat_functions/index.js
+++ libs/chat_functions/index.js
@@ -1,5 +1,5 @@
 var chat_functions = {};
-chat_functions.cancer_mode = require('./cancer_mode.js');
+chat_functions.cancer_mode = require('./word_ban.js');
 chat_functions.chat_handler = require('./chat_handler.js');
 
 //Get all the functions.  If we implement these like they have an interface we can run everything in the folder
@@ -9,10 +9,8 @@ var functions = Object.getOwnPropertyNames(chat_functions).filter(function (p) {
 
 
 module.exports = function(twitch_client, channel, user, message){
-	console.log("GOT HERE");
 	console.log("functions loaded: "+functions);
 	functions.forEach(function(element){
-		console.log("AND HERE");
 		chat_functions[element](twitch_client, channel, user, message);
 	});
 }
